bb8e33b5216ed8f9d306a903c98558252c2363e7
Tobias Prisching <tobias.prisching@icloud.com>
2023-10-05 23:53:35 +0200

    Create PubSubPattern.swift

---
+++ rawViewer/rawViewer/PubSubPattern.swift
@@ -0,0 +1,36 @@
+//
+//  PubSubPattern.swift
+//  rawViewer
+//
+//  Created by Tobias Prisching on 05.10.23.
+//
+
+import Foundation
+
+class Publisher
+{
+    var observers: Set<AnyHashable> = [];
+    
+    func send()
+    {
+        for item in self.observers
+        {
+            (item as? Observer)?.update()
+        }
+    }
+    
+    func insert<T>(_ item: T) where T: Observer, T: Hashable
+    {
+        self.observers.insert(AnyHashable(item))
+    }
+    
+    func remove<T>(_ item: T) where T: Observer, T: Hashable
+    {
+        self.observers.remove(AnyHashable(item))
+    }
+}
+
+protocol Observer
+{
+    func update();
+}
